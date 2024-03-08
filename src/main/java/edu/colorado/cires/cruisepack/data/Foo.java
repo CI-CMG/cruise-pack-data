a533475021eb9d5862d0ab49a99dc11277dbd121
Chris Slater <6004777+electricsam@users.noreply.github.com>
2024-03-08 13:56:13 -0700

    try release

---
+++ src/main/java/edu/colorado/cires/cruisepack/data/Foo.java
@@ -0,0 +1,11 @@
+package edu.colorado.cires.cruisepack.data;
+
+/**
+ * This is a placeholder
+ */
+public class Foo {
+
+  public String bar() {
+    return "bar";
+  }
+}
---
+++ src/test/java/edu/colorado/cires/cruisepack/data/FooTest.java
@@ -0,0 +1,13 @@
+package edu.colorado.cires.cruisepack.data;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class FooTest {
+
+  @Test
+  public void testBar() {
+    assertEquals("bar", new Foo().bar());
+  }
+}
